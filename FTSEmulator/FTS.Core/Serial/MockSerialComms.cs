17349d815923e02d8156727b234a29fdb9f7273d
Isaac Candido <isaac.guimaraescandido@gmail.com>
2021-02-18 18:03:07 -0300

    Update MockSerialComms.cs

--- FTSEmulator/FTS.Core/Serial/MockSerialComms.cs
+++ FTSEmulator/FTS.Core/Serial/MockSerialComms.cs
@@ -1,15 +1,25 @@
-﻿namespace FTS.Core
+﻿using System.IO.Ports;
+
+namespace FTS.Core
 {
     public class MockSerialComms : ISerial
     {
         public event OnTryConnect TryConnect;
-        public event OnConnectSuccessfull ConnectSuccessfull;
+        public event OnConnectSuccessful ConnectSuccessful;
         public event OnConnectFailure ConnectFailure;
+        public event OnEngravingToggle EngravingToggle;
 
         public bool IsOpen { get; set; }
 
+        public SerialPort Serial { get; } = new SerialPort("MOCK_COM", 9600, Parity.None, 8, StopBits.One);
+
         // mock serial class, for testing
-        public void Move(byte Command) { }
+        public void Move(/*byte Command*/) { }
         public bool Open() => true;
+        public void Listen() 
+        {
+            TryConnect?.Invoke(new SerialConnectEventArgs());
+            ConnectSuccessful?.Invoke(new SerialConnectEventArgs());
+        }
     }
 }
