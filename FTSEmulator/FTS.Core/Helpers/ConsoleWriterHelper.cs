2348fda0f46115946a7a9cedc6a7cb7ec6884927
Isaac Candido <isaac.guimaraescandido@gmail.com>
2021-02-24 20:08:40 -0300

    Create ConsoleWriterHelper.cs
    Created a separated class so I can control flow of onscreen writting

---
+++ FTSEmulator/FTS.Core/Helpers/ConsoleWriterHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace FTS.Core
+{
+    public class ConsoleWriter
+    {
+        object lockObj;
+        public ConsoleWriter(object lockObj)
+        {
+            if (lockObj is null) throw new Exception("Invalid lock object");
+            this.lockObj = lockObj;
+        }
+
+        public void HorizontalLine(PointI position, int len)
+        {
+            lock (lockObj)
+            {
+                try
+                {
+                    Console.SetCursorPosition(position.X, position.Y);
+                    Console.Write(new string('-', len));
+                }
+                catch { return; }
+            }
+        }
+
+        public void VerticalLine(PointI position, int height)
+        {
+            lock (lockObj)
+            {
+                try
+                {
+                    for (int i = 0; i < height; i++)
+                    {
+                        Console.SetCursorPosition(position.X, position.Y++);
+                        Console.Write('|');
+                    }
+                }
+                catch { return; }
+            }
+        }
+
+        // Console Writer, centralized, so onscreen texts aren't going to get messy.
+        public void WriteOnConsole(string message, PointI location, int cleanLen, bool showCursor = false)
+        {
+            lock (lockObj)
+            {
+                Console.CursorVisible = showCursor;
+
+                Console.SetCursorPosition(location.X, location.Y);
+                Console.Write(new string(' ', cleanLen));
+                Console.SetCursorPosition(location.X, location.Y);
+                Console.Write(message);
+
+                if (Console.CursorVisible) Console.CursorVisible = false;
+            }
+        }
+        public void CleanLine(PointI pos, int cleanLen)
+        {
+            WriteOnConsole(string.Empty, pos, cleanLen);
+            Console.SetCursorPosition(pos.X, pos.Y);
+        }
+    }
+}
