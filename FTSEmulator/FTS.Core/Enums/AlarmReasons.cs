f805de04e2db1f8095ef9db3469d4ec7a23ca281
Isaac Candido <isaac.guimaraescandido@gmail.com>
2021-02-25 02:16:08 -0300

    Update AlarmReasons.cs

--- FTSEmulator/FTS.Core/Enums/AlarmReasons.cs
+++ FTSEmulator/FTS.Core/Enums/AlarmReasons.cs
@@ -5,8 +5,5 @@
         // 1XX Move and position related
         UnkownCurrentLocation = 101,
         MoveOutOfBounds = 102,
-
-        // 9xx connection-related
-        ControllerDisconnected = 901
     }
 }
