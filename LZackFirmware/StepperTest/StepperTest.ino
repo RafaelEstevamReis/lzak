6c1752ca17ff51e92e813beb81cedc3fe4a0e6d7
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-02-25 21:15:52 -0300

    Create StepperTest.ino

---
+++ LZackFirmware/StepperTest/StepperTest.ino
@@ -0,0 +1,29 @@
+#define DIR_PIN  4
+#define STEP_PIN 5
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(DIR_PIN, OUTPUT);
+  pinMode(STEP_PIN, OUTPUT);
+}
+
+int steps = 0;
+int dir = 0;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(100);
+
+  if(steps++ > 50) {
+    steps = 0;
+    
+    //if(dir == 1) dir = 0;
+    //else dir = 1;
+  }
+  digitalWrite(DIR_PIN, dir);
+  delay(1);
+  digitalWrite(STEP_PIN, 1);
+  delay(1);
+  digitalWrite(STEP_PIN, 0);
+  
+}
