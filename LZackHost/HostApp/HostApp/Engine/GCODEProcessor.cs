52df08e6d7e8fcee907a8365c84ff855b7dd1ac4
Isaac Candido <isaac.guimaraescandido@gmail.com>
2021-03-23 15:07:55 -0300

    Added files for processing, not implemented

---
+++ LZackHost/HostApp/HostApp/Engine/GCODEProcessor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HostApp
+{
+    public static class GCODEProcessor
+    {
+        public static string ToGCODE(this bool[,] map)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LZackHost/HostApp/HostApp/Engine/MapProcessor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Drawing;
+
+namespace HostApp
+{
+    public static class MapProcessor
+    {
+        public static bool[,] ToBooleanArray(this Image image)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
