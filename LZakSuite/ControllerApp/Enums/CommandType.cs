ae7ebb2aec101a4a4a247ccfd153aeef11fb3795
Isaac Candido <isaac.guimaraescandido@gmail.com>
2021-04-15 20:59:49 -0300

    CommandType for GCODE interpretation

---
+++ LZakSuite/ControllerApp/Enums/CommandType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControllerApp.Enums
+{
+    public enum CommandType
+    {
+        X, // X axis instruction
+        Y, // Y Axis instruction
+        Z, // Z axis instruction
+        M, // Turn Z on or OFF
+        G  // Move command
+    }
+}
