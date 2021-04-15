9d04d72b7ab81b31e20597bc04b936d0394a639c
Isaac Candido <isaac.guimaraescandido@gmail.com>
2021-04-15 20:57:43 -0300

    Added testcases (unfinished)

---
+++ LZakSuite/ControllerApp.Tests/HelperTests.cs
@@ -0,0 +1,23 @@
+﻿using ControllerApp.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ControllerApp.Tests
+{
+    public class HelperTests
+    {
+        [Fact]
+        public static void StringToFloat_Valid()
+        {
+            string value = "3.22";
+
+            var converted = value.StringToFloat();
+
+            Assert.Equal(3.22, converted);
+        }
+    }
+}
---
+++ LZakSuite/ControllerApp.Tests/RuntimeResourcesTests.cs
@@ -0,0 +1,15 @@
+﻿using ControllerApp.ControllerCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ControllerApp.Tests
+{
+    public class RuntimeResourcesTests
+    {
+
+    }
+}
